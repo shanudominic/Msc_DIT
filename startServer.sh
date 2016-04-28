b2511dc1f930cb4ad0eb90332b3df49e9f45d57b
Shanu Dominic <shanudominic@gmail.com>
2016-04-28 07:19:48 +0100

    Renamed file

--- startServer.sh
+++ startServer.sh
@@ -4,6 +4,7 @@ clear;
 a=$(ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}');
 echo $a;
 /usr/bin/google-chrome-stable "http://$a:8080/ITunes_Lab";
-/bin/bash /usr/local/share/jboss/bin/standalone.sh -Djboss.bind.address="$a" -Djboss.bind.address.management="$a"
+/bin/bash /usr/local/share/jboss/bin/standalone.sh -Djboss.bind.address="$a" -Djboss.bind.address.management="$a";
+
 
 
